ae242f54a5e71380a483eae9a87c3c8bfcf05f81
katerynapolonska <kapolo19961996@gmail.com>
2018-11-09 16:43:14 +0200

    Compliance of standards rules for class and rules for method.Adding new class.

--- TestRunner.py
+++ TestRunner.py
@@ -7,6 +7,3 @@ basicSuite.addTest(unittest.makeSuite(TestforUS1.Tests.TestTC1.TestTC1))
 basicSuite.addTest(unittest.makeSuite(TestforUS1.Tests.TestTC2.TestTC2))
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(basicSuite)
-
-
-
