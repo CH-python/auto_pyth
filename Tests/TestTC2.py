8bc08b231529c330b5f030068ea3e8fef408e16a
sefoo13 <sviatuy_93@mail.ru>
2018-11-09 04:18:45 +0200

    2 Automate Tests

---
+++ Tests/TestTC2.py
@@ -0,0 +1,68 @@
+import unittest
+from selenium import webdriver
+from TestforUS1.Pages.WelcomePage import Welcome
+from TestforUS1.Pages.SignInPage import Sign_In
+from TestforUS1.Pages.MainUserPage import MainUserPage,popupItemPayment,popupEasyPay,additionalFields,additionalClose
+from TestforUS1.Pages.PaymentsPage import PaymentsPage
+from selenium.webdriver.common.by import By
+
+class TestTC2(unittest.TestCase):
+
+    def setUp(self):
+        # self.driver = webdriver.Chrome()
+        self.driver = webdriver.Firefox()
+        self.driver.get("http://localhost:8080/")
+
+    def test_getToSignIn(self):
+        welcome = Welcome(self.driver)
+        welcome.sign_in.click()
+
+        signIn = Sign_In(self.driver)
+        signIn.email.send_keys("user1@gmail.com")
+        signIn.password.send_keys("Qwerty13")
+        signIn.submit.click()
+
+        self.driver.implicitly_wait(10)
+        userPage = MainUserPage(self.driver)
+        userPage.itemPayments.click()
+
+        self.driver.implicitly_wait(10)
+        popupDetails = popupItemPayment(self.driver)
+        popupDetails.btnDetails.click()
+        popupDetails.btnPay.click()
+        popupDetails.inputPaymentSum.send_keys("2")
+        popupDetails.btnDownloadCheck.click()
+        popupDetails.btnProceed.click()
+
+        self.driver.switch_to.frame(self.driver.find_element_by_tag_name("iframe"))
+        self.driver.implicitly_wait(10)
+        popEasyPay = popupEasyPay(self.driver)
+        popEasyPay.cardNumberInput.send_keys("4242424242424242")
+        popEasyPay.dateCardInput.send_keys("012019")
+        popEasyPay.cvNumberInput.send_keys("333")
+        popEasyPay.mailInput.send_keys("sviatuy93@gmail.com")
+        popEasyPay.btnRememberMe.click()
+
+        self.driver.implicitly_wait(10)
+        fields = additionalFields(self.driver)
+        fields.zipCodeInput.send_keys("58022")
+        fields.PhoneInput.send_keys("991214424")
+
+        popEasyPay.btnPay.click()
+
+        self.driver.implicitly_wait(10)
+        close = additionalClose(self.driver)
+        close.btnClose.click()
+        self.driver.implicitly_wait(10)
+        popEasyPay.btnPay.click()
+
+        self.driver.switch_to.default_content()
+
+        # self.driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        # currentURL = self.driver.Url
+
+    def TearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
--- locators.py
+++ locators.py
@@ -11,8 +11,26 @@ class Locator(object):
 
     #main user page locators
     payments="//*[@id='sidebar-menu']/div/ul/li[3]/a"
+
+    #popup payments
+    pay="pay"
     details="//*[@id='historyTable']/tbody/tr[2]/td[3]/button"
-    titlePopup='//*[@id="myModalLabel"]/span'
+    titlePopup="//*[@id='myModalLabel']/span"
+    paymentSum="payment-sum-input"
+    downloadCheck="download-check-label"
+    proceed="payment-proceed"
+
+    #popup payments: EasyPay
+    wayToFields = "/html/body/div/section/span/div/div/main/form/div/div/div/div/div/div/div"
+    mailInput= wayToFields + "/div/div/div/fieldset/span/div/div/input"
+    cardNumberInput= wayToFields + "/fieldset/div/div/span/span/div/div/input"
+    dateCardInput= wayToFields + "/fieldset/div/div/div/div/input"
+    cvNumberInput=wayToFields + "/fieldset/div/div/div[2]/div/input"
+    zipCodeInput=wayToFields + "/fieldset/div/div/div/div/div/div/input"
+    btnRememberMe=wayToFields + "/div/fieldset/span"
+    PhoneInput=wayToFields + "/div/div/div/fieldset/fieldset/span/div/div/input"
+    btnPay="Button-animationWrapper-child--primary"
+    btnClose="Header-navBack"
 
     #payments page locators
     first_utility_details="#historyTable > tbody > tr:nth-child(1) > td:nth-child(3) > button"
