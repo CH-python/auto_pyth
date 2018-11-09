6ad9f01ec3f189f051788f852d0e63d203af7e74
sefoo13 <sviatuy_93@mail.ru>
2018-11-09 04:21:24 +0200

    2 Automate Tests

--- TestRunner.py
+++ TestRunner.py
@@ -3,7 +3,7 @@ import TestforUS1.Tests.TestTC1
 import TestforUS1.Tests.TestTC2
 
 basicSuite = unittest.TestSuite()
-# basicSuite.addTest(unittest.makeSuite(TestforUS1.Tests.TestTC1.TestTC1))
+basicSuite.addTest(unittest.makeSuite(TestforUS1.Tests.TestTC1.TestTC1))
 basicSuite.addTest(unittest.makeSuite(TestforUS1.Tests.TestTC2.TestTC2))
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(basicSuite)
