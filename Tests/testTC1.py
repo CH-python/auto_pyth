fe0e2f22905e752095a152f53a6f00e4d9c52323
katerynapolonska <kapolo19961996@gmail.com>
2018-11-11 13:17:59 +0200

    Adding screenShot file. Adding function that making screenshots to testTC1

--- Tests/testTC1.py
+++ Tests/testTC1.py
@@ -4,17 +4,23 @@ from TestforUS1.Pages.welcomePage import Welcome
 from TestforUS1.Pages.signInPage import SignIn
 from TestforUS1.Pages.mainUserPage import MainUserPage
 from TestforUS1.Pages.paymentsPage import PaymentsPage,PopupItemPayment
+from TestforUS1.screenShot import SS
+
 
 class TestTC1(unittest.TestCase):
 
     def setUp(self):
+
         # self.driver = webdriver.Chrome()
         self.driver = webdriver.Firefox()
         self.driver.get("http://localhost:8080/")
 
     def testGetToSignIn(self):
+        ss = SS(self.driver)
         welcome = Welcome(self.driver)
+        ss.ScreenShot('/testTC1/screen1.png')
         welcome.signIn.click()
+        ss.ScreenShot('/testTC1/screen2.png')
 
         signIn = SignIn(self.driver)
         signIn.email.send_keys("user1@gmail.com")
@@ -23,11 +29,14 @@ class TestTC1(unittest.TestCase):
 
         self.driver.implicitly_wait(10)
         userPage = MainUserPage(self.driver)
+        ss.ScreenShot('/testTC1/screen3.png')
         userPage.itemPayments.click()
 
+        ss.ScreenShot('/testTC1/screen4.png')
         paymentPage = PaymentsPage(self.driver)
         paymentPage.btnDetails.click()
 
+        ss.ScreenShot('/testTC1/screen5.png')
         popupDetails = PopupItemPayment(self.driver)
         actual = popupDetails.titleDetails.text
         self.assertEquals(actual, "Utility details");
