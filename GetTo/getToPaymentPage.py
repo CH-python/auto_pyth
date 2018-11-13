31b086443d9036f16033977fc5d330c72e52edb3
Seffo13 <Sviatuy_93@mail.ru>
2018-11-13 17:18:18 +0200

    Delete getToPaymentPage.py

--- GetTo/getToPaymentPage.py
+++
@@ -1,8 +0,0 @@
-from TestforUS1.Pages.mainUserPage import MainUserPage
-
-class getToPaymentPage(object):
-
-    def __init__(self, driver):
-        self.driver = driver
-        userPage = MainUserPage(self.driver)
-        userPage.itemPayments.click()
