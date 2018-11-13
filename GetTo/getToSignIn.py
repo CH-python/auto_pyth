cc51c898404c2cf217edf9d679084b815276ebb6
Seffo13 <Sviatuy_93@mail.ru>
2018-11-13 17:18:48 +0200

    Delete getToSignIn.py

--- GetTo/getToSignIn.py
+++
@@ -1,9 +0,0 @@
-from TestforUS1.Pages.welcomePage import Welcome
-
-class getToSignInPage(object):
-
-    def __init__(self, driver):
-        self.driver = driver
-
-        welcome = Welcome(self.driver)
-        welcome.signIn.click()
